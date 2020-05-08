33d377002cd28c753aa0891b5267962801a28e66
xbrunner <62433284+xbrunner@users.noreply.github.com>
2020-05-08 16:18:11 +0200

    Version 1: 5 sections

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
+<!--
+  ATLAS TEMPLATE
+  Multimedia Cartography
+  Institute of Cartography
+  Spring semester 2020
+  hbaer@ethz.ch
+-->
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <!-- Contents of the application window -->
+  <v-app>
+    <v-app-bar app color="green darken-4" dark flat>
+
+      <!-- Navigation icon -->
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Atlas of Sustainable Development</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Use of Vue Router to link dieffernet content.
+           See router/index.js to learn how to add new components. -->
+      <v-btn text to="/">Welcome</v-btn>
+      <v-btn text to="/content">Content</v-btn>
+      <v-btn text to="/imprint">Imprint</v-btn>
+
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary clipped></v-navigation-drawer>
+
+    <v-content>
+      <!-- This is where the selected component is displayed -->
+      <router-view/>
+    </v-content>
+
+  </v-app>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    drawer: false
+  }),
+};
+</script>
